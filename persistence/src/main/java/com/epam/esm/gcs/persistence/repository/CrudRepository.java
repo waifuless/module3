b7d62e8eb969fe9e779f04fe20991891255c4383
Vladimir <vova.prusenok@gmail.com>
2022-01-14 14:39:03 +0300

    refactor UpdateService/Repository to CrudRepostirory, extended CrdService/Repository

--- persistence/src/main/java/com/epam/esm/gcs/persistence/repository/CrudRepository.java
+++ persistence/src/main/java/com/epam/esm/gcs/persistence/repository/CrudRepository.java
@@ -1,6 +1,6 @@
 package com.epam.esm.gcs.persistence.repository;
 
-public interface UpdateRepository<T> {
+public interface CrudRepository<T> extends CrdRepository<T> {
 
     void update(T model);
 }
