d6479c0e60eb25cb5681c1793e322ff7fe62a608
Vladimir <vova.prusenok@gmail.com>
2022-01-27 13:25:30 +0300

    make docs for services

--- business/src/main/java/com/epam/esm/gcs/business/service/TagService.java
+++ business/src/main/java/com/epam/esm/gcs/business/service/TagService.java
@@ -4,11 +4,31 @@ import com.epam.esm.gcs.business.dto.TagDto;
 
 import java.util.List;
 
+/**
+ * Service interface that contains all methods for interaction with TagDto
+ */
 public interface TagService extends CrdService<TagDto> {
 
+    /**
+     * Finds all TagDtos
+     *
+     * @return List of all TagDtos
+     */
     List<TagDto> findAll();
 
+    /**
+     * Checks the existence of TagDto with some name
+     *
+     * @param name - name for search
+     * @return true - if TagDto with @param name exists, false - if TagDto with @param name does not exist
+     */
     boolean existsByName(String name);
 
+    /**
+     * Checks the existence of TagDto by name. If it doesn't exist, creates it.
+     *
+     * @param name - name for search
+     * @return - found or created TagDto
+     */
     TagDto findOrCreate(String name);
 }
